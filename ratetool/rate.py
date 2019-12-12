d8405e3bcd870ababcb5f15d2436767e5d3e9404
zhengshanzhou <zzs2260736@126.com>
2019-12-12 14:20:06 +0800

    利率

--- ratetool/rate.py
+++ ratetool/rate.py
@@ -55,14 +55,14 @@ def total_payment(principal, annual_rate, total_month, month):
     return total_principal
 
 
-def cal(money, proportion, float_rate, total_month):
+def cal(money, proportion, Benchmark_rate, float_rate, total_month):
     '''
     :param money: 可使用资金
     :param proportion: 首付比例
     :param float_rate:  利率上浮
     '''
     total_price = money / proportion
-    rate = 0.048 * (1 + float_rate)
+    rate = Benchmark_rate * (1 + float_rate)
     print("首付款%s 首付比率%s 利率 %s" % (money, proportion, str(rate * 100) + "%"))
     print("可购买房屋总价上限 %s 按 %d 年分期计算" % (total_price, total_month / 12))
     total_rate(total_price - money, rate, total_month)
@@ -71,20 +71,25 @@ def cal(money, proportion, float_rate, total_month):
 def buy(money, loan_life):  # 计算 首套 和 二套的情况
     print("可使用资金 %s" % money)
     print("二套".center(30, "-"))
-    cal(money, 0.7, 0.25, loan_life * 12)
+    cal(money, 0.7, 0.048, 0.25, loan_life * 12)
     print("".center(30, "-"))
-    cal(money, 0.6, 0.25, loan_life * 12)
+    cal(money, 0.6, 0.048, 0.25, loan_life * 12)
     print('首套'.center(30, "-"))
-    cal(money - 50 * 10000 + total_payment(50 * 10000, 0.048 * (1 + 0.05), 30 * 12, 12 * 2), 0.3, 0.2, loan_life * 12)
+    cal(money - 50 * 10000 + total_payment(50 * 10000, 0.048 * (1 + 0.05), 30 * 12, 12 * 2), 0.3, 0.048, 0.2,
+        loan_life * 12)
 
 
-flag = True
-while (flag):
-    print("请输入可用于购房的资金(万元)")
-    m = float(input(">>>"))
-    print("贷款年限")
-    y = int(input(">>>"))
-    buy(m * 10000, y)
-    print("")
-    flag = input("按n/N退出或继续").lower() != "n"
-    print("")
+# flag = True
+# while (flag):
+#     print("请输入可用于购房的资金(万元)")
+#     m = float(input(">>>"))
+#     print("贷款年限")
+#     y = int(input(">>>"))
+#     buy(m * 10000, y)
+#     print("")
+#     flag = input("按n/N退出或继续").lower() != "n"
+#     print("")
+
+
+# cal(70 * 10000, 70 / 110, 0.0325, 0.1, 20 * 12)
+cal(70 * 10000, 70 / 110, 0.048, 0.2, 20 * 12)
